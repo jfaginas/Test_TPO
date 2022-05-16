2498cad9762291e4e91dfa03c41eddc2812130d3
jfaginas <jfaginas@gmail.com>
2022-05-16 14:16:21 -0300

    Header en js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+const cad = `
+    <nav class="nav">
+        <img src="./img/logo-szabo-sss.png" class="logo nav-link" alt="Logo Coghlan">
+        <button class="nav-toggle" aria-label="Abrir menú">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <ul class="nav-menu">
+            <li class="nav-menu-item">
+                <a href="index.html" class="nav-menu-link nav-link">El Barrio</a>            
+            </li>
+            <li class="nav-menu-item">
+                <a href="arte.html" class="nav-menu-link nav-link">Arte</a>
+            </li>
+            <li class="nav-menu-item">
+                <a href="contacto.html" class="nav-menu-link nav-link nav-menu-link_active">Contacto</a>
+            </li>
+        </ul>
+    </nav>
+`
+document.getElementById("idheader").innerHTML=cad;
+
+
 const navToggle = document.querySelector(".nav-toggle")
 const navMenu = document.querySelector(".nav-menu")
 
@@ -12,3 +34,6 @@ navToggle.addEventListener("click", () => {
 });
 
 // Header -----------------------------------
+
+
+
